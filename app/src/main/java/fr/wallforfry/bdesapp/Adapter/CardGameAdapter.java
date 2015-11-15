36b6097ddee51f1276cf6d71bc0c7e49d468ef2f
wallforfry <wallforfry@gmail.com>
2015-11-15 12:16:11 +0100

    commit Cards News test

--- app/src/main/java/fr/wallforfry/bdesapp/Adapter/CardGameAdapter.java
+++ app/src/main/java/fr/wallforfry/bdesapp/Adapter/CardGameAdapter.java
@@ -16,32 +16,32 @@ import fr.wallforfry.bdesapp.R;
 */
 public class CardGameAdapter extends RecyclerView.Adapter<CardGameViewHolder> {
 
-List<CardGameObject> list;
+    List<CardGameObject> list;
 
-//ajouter un constructeur prenant en entrée une liste
-public CardGameAdapter(List<CardGameObject> list) {
-    this.list = list;
-}
+    //ajouter un constructeur prenant en entrée une liste
+    public CardGameAdapter(List<CardGameObject> list) {
+        this.list = list;
+    }
 
-//cette fonction permet de créer les viewHolder
-//et par la même indiquer la vue à inflater (à partir des layout xml)
-@Override
-public CardGameViewHolder onCreateViewHolder(ViewGroup viewGroup, int itemType) {
-    View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_cards,viewGroup,false);
-    return new CardGameViewHolder(view);
-}
+    //cette fonction permet de créer les viewHolder
+    //et par la même indiquer la vue à inflater (à partir des layout xml)
+    @Override
+    public CardGameViewHolder onCreateViewHolder(ViewGroup viewGroup, int itemType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_cards,viewGroup,false);
+        return new CardGameViewHolder(view);
+    }
 
-//c'est ici que nous allons remplir notre cellule avec le texte/image de chaque MyObjects
-@Override
-public void onBindViewHolder(CardGameViewHolder myViewHolder, int position) {
-    CardGameObject myObject = list.get(position);
-    myViewHolder.bind(myObject);
-}
+    //c'est ici que nous allons remplir notre cellule avec le texte/image de chaque MyObjects
+    @Override
+    public void onBindViewHolder(CardGameViewHolder myViewHolder, int position) {
+        CardGameObject myObject = list.get(position);
+        myViewHolder.bind(myObject);
+    }
 
-@Override
-public int getItemCount() {
-    return list.size();
-}
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
 
 }
 
